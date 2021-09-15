6f02101a38eff6979a6d9c8b1d25be5e1c8cee99
dipesh2901 <67797922+dipesh2901@users.noreply.github.com>
2021-09-16 00:34:25 +0530

    Task54- Microservices(Temperature)

---
+++ api-gateway/src/test/java/com/neosoft/microservices/naming/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosoft.microservices.naming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
